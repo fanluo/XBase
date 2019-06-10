e879dbb64f4d0ef952be2e1f979bcc67a65067f0
江海洋 <1885690754@163.com>
2019-06-10 22:29:31 +0800

    rxpermission

--- app/src/main/java/com/allens/allenstools/test_act/PermissionAct.java
+++ app/src/main/java/com/allens/allenstools/test_act/PermissionAct.java
@@ -1,10 +1,15 @@
 package com.allens.allenstools.test_act;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.allens.allenstools.R;
 import com.allens.lib_base.base.BaseActivity;
+import com.allens.lib_base.log.LogHelper;
+
+import io.reactivex.functions.Consumer;
 
 @Route(path = "/act/permission")
 public class PermissionAct extends BaseActivity {
@@ -13,9 +18,34 @@ public class PermissionAct extends BaseActivity {
         return R.layout.activity_permission;
     }
 
+    @SuppressLint("CheckResult")
     @Override
     protected void initView(Bundle savedInstanceState) {
         $(R.id.button).setOnClickListener(v -> {
+            permissionRequest(Manifest.permission.CAMERA)
+                    .subscribe(granted -> {
+                        if (granted) {
+                            LogHelper.i("给了权限");
+                            toast("给了权限");
+                        } else {
+                            LogHelper.i("拒绝权限");
+                            toast("拒绝权限");
+                        }
+                    });
+        });
+
+        $(R.id.button2).setOnClickListener(v -> {
+            permissionRequestEach(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .subscribe(permission -> {
+                        if (permission.granted) {
+                            // `permission.name` is granted !
+                        } else if (permission.shouldShowRequestPermissionRationale) {
+                            // Denied permission without ask never again
+                        } else {
+                            // Denied permission with ask never again
+                            // Need to go to the settings
+                        }
+                    });
         });
     }
 
